43806eec9203cca5eb57b08a4e14cd7cf5b5ce38
jamila455 <jrahimi@mail.mccneb.edu>
2025-09-16 14:43:49 -0500

    Fixed hourlyemlpoyee, saleryemployee and WeeklyPayCalculator.java to validate positive hours and time calculation.

--- src/test/WeeklyPayCalculator.java
+++
@@ -1,8 +0,0 @@
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-// Annotation for weekly pay methods
-@Retention(RetentionPolicy.RUNTIME)
-public @interface WeeklyPayCalculator {
-}
